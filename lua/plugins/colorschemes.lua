09d22d87293b9640101d50e4ca2a74aa1c0660ff
Dat Le <dle.dev@outlook.com>
2026-03-19 05:47:05 -0400

    remove unused colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,13 +1,8 @@
 vim.pack.add({
   "https://github.com/catppuccin/nvim",
   "https://github.com/rebelot/kanagawa.nvim",
-  "https://github.com/EdenEast/nightfox.nvim",
   "https://github.com/folke/tokyonight.nvim",
-  "https://github.com/rose-pine/neovim",
-  "https://github.com/olimorris/onedarkpro.nvim",
   "https://github.com/sainnhe/gruvbox-material",
-  "https://github.com/navarasu/onedark.nvim",
-  "https://github.com/projekt0n/github-nvim-theme",
 })
 
 vim.pack.add({ { src = "https://codeberg.org/evergarden/nvim.git", name = "evergarden" } })

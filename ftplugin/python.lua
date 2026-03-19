30dcabd7552e51656ef6553b177ce22727f34406
Dat Le <dle.dev@outlook.com>
2026-03-19 05:49:44 -0400

    fix nvim-treesitter config
    With neovim 0.12, nvim-treesitter config became very weird. I can't call
    `require("nvim-treesitter.configs").setup()` and pass in options
    anymore. Instead I can manually call install to ensure certain parsers
    are installed, then I need to add `/ftplugin` to the config root and
    have a file for each <filetype> that configures it individually. Auto
    commands didn't seem to work either. This is also dependent on
    `tree-sitter-cli` being installed by the system package manager.

---
+++ ftplugin/python.lua
@@ -0,0 +1,5 @@
+vim.treesitter.start()
+
+-- -- nvim-treesitter indentation for python seems unpredictable, so omit
+-- -- the following line and just rely on native indentation
+-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,30 @@
+-- maybe due to neovim 0.12, but nvim-treesitter setup needs to be as follows
+-- - install tree-sitter-cli with package manager
+-- - create ftplugin/<filetype>.lua for each filetype
+-- - add `vim.treesitter.start()` at minimum so that nvim-treesitter highlighting
+--   starts when file is opened
+-- - include any additional options like folds or indentation
+-- autocmd to start nvim-treesitter on buffer open based on filetype wouldn't work for whatever reason
+
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
 
-require("nvim-treesitter").setup({
-  ensure_installed = {
-    "bash",
-    "c",
-    "diff",
-    "ini",
-    "json",
-    "lua",
-    "luadoc",
-    "markdown",
-    "markdown_inline",
-    "python",
-    "query",
-    "regex",
-    "rust",
-    "toml",
-    "vim",
-    "vimdoc",
-    "yaml",
-  },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    -- some languages depend on vim's regex highlighting system for indent rules
-    -- if you experience weird indent issues, add language to list of
-    -- additional_vim_regex_highlighting and disabled languages for indent
-    additional_vim_regex_highlighting = { "python" },
-  },
-  indent = { enable = true, disable = { "python" } },
+require("nvim-treesitter").setup()
+require("nvim-treesitter").install({
+  "bash",
+  "c",
+  "diff",
+  "ini",
+  "json",
+  "lua",
+  "luadoc",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "query",
+  "regex",
+  "rust",
+  "toml",
+  "vim",
+  "vimdoc",
+  "yaml",
 })
